38f001ca66d3dcc28a076a299df2841e2a8209ea
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-26 17:29:06 -0300

    Implements MainWindowController

---
+++ Sample/MainWindowController.swift
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.swift
+//  Sample
+//
+//  Created by Lantean on 1/26/21.
+//
+
+import Foundation
+import AppKit
+
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        window?.center()
+    }
+}
