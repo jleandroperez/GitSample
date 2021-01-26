3d35165cdbfb8d2d62c881a8e667c8999fad794b
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-26 17:20:19 -0300

    Initial commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Lantean on 1/26/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
